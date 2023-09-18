143dbef549f929bcda85bc2d2eb4b3e872608721
GABRIEL ALBUQUERQUE DE MOURA SILVA <2560245@fmu.interno>
2023-09-18 09:56:53 -0300

    18/09/2023 x2

--- 2-SegundoSemestre/1-TecnicasProgramacao/1-Aulas/5-Aula 5/1-Exemplos/main.c
+++ 2-SegundoSemestre/1-TecnicasProgramacao/1-Aulas/5-Aula 5/1-Exemplos/main.c
@@ -2,9 +2,30 @@
 #include <stdlib.h>
 #include <locale.h>
 
-
 int main() {
+	int valores[5];
+	int pInicial, pTeste, aux, i;
 	setlocale(LC_ALL,"");
-	printf("\n Suiça");
+	for(i=0; i<5;i++){
+		printf("\nDigite um número (%d): ", i);
+		scanf("%d",&valores[i]);
+	}
+	 
+	//Método bubble sort
+	
+	for(pInicial=0;pInicial<4;pInicial++){
+		for(pTeste= pInicial+1; pTeste<5; pTeste++){
+			if(valores[pInicial]>valores[pTeste]){
+			aux=valores[pInicial];
+			valores[pInicial] = valores[pTeste];
+			valores[pTeste] = aux;
+			}
+ 		}
+	}
+	printf("\nOrdenando: ");
+	for(i=0; i<5;i++){
+		printf("\n %d ", valores[i]);
+
+	}
 	return 0;
 }
