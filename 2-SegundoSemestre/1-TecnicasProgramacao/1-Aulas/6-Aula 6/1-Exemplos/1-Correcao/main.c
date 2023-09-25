180038c7f11ea57298fef093f640111b11075a78
GABRIEL ALBUQUERQUE DE MOURA SILVA <2560245@fmu.interno>
2023-09-25 09:29:09 -0300

    25/09/2023 x2

--- 2-SegundoSemestre/1-TecnicasProgramacao/1-Aulas/6-Aula 6/1-Exemplos/1-Correcao/main.c
+++ 2-SegundoSemestre/1-TecnicasProgramacao/1-Aulas/6-Aula 6/1-Exemplos/1-Correcao/main.c
@@ -2,9 +2,23 @@
 #include <stdlib.h>
 #include <locale.h>
 
-
 int main() {
+	int vet[10], i=0;
+	void imprimeInv(int vet[]);
 	setlocale(LC_ALL,"");
-	printf("\n ");
+	for(i=0;i<=9;i++){
+		printf("\nDigite um número: (%d)",i+1);
+		scanf("%d",&vet[i]);
+	}
+	imprimeInv(vet);
 	return 0;
 }
+void imprimeInv(int vet[]){
+	int i;
+	printf("\nImprimindo inverso: ");
+	for(i=9;i>=0;i--){
+		printf("\n  %d (%d)",vet[i], i+1);
+	}
+	return;
+}
+
