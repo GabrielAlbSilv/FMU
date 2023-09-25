08bfe8e7f1e4d549db09373eec0b8aadb2b2b369
GABRIEL ALBUQUERQUE DE MOURA SILVA <2560245@fmu.interno>
2023-09-25 10:07:34 -0300

    25/09/2023 x3

--- 2-SegundoSemestre/1-TecnicasProgramacao/1-Aulas/6-Aula 6/1-Exemplos/1-Correcao/main.c
+++ 2-SegundoSemestre/1-TecnicasProgramacao/1-Aulas/6-Aula 6/1-Exemplos/1-Correcao/main.c
@@ -3,22 +3,45 @@
 #include <locale.h>
 
 int main() {
-	int vet[10], i=0;
+	int vet[10], i, aux, ini, test;
 	void imprimeInv(int vet[]);
+	int maiorVal(int vet[]);
+	int menorVal(int vet[]);
 	setlocale(LC_ALL,"");
 	for(i=0;i<=9;i++){
-		printf("\nDigite um número: (%d)",i+1);
+		printf("\nDigite um número: (%d): ",i+1);
 		scanf("%d",&vet[i]);
 	}
+	for(ini=0;ini<9;ini++){
+		for(test= ini+1; test<10; test++){
+			if(vet[ini]>vet[test]){
+			aux=vet[ini];
+			vet[ini] = vet[test];
+			vet[test] = aux;
+			}
+ 		}
+	}
+	printf("\n \nOrdenando:");
+	for(i=0;i<=9;i++){
+		printf(" %d",vet[i]);
+	}
 	imprimeInv(vet);
+	printf("\nMaior valor: %d", maiorVal(vet));
+	printf("\nMaior valor: %d", menorVal(vet));
 	return 0;
 }
 void imprimeInv(int vet[]){
 	int i;
 	printf("\nImprimindo inverso: ");
 	for(i=9;i>=0;i--){
-		printf("\n  %d (%d)",vet[i], i+1);
+		printf(" %d ",vet[i]);
 	}
 	return;
+}
+int maiorVal(int vet[]){
+	return vet[9];
+}
+int menorVal(int vet[]){
+	return vet[0];
 }
 
