a7d72874b30eef1450c651f3690f9c937692b89b
GABRIEL ALBUQUERQUE DE MOURA SILVA <2560245@fmu.interno>
2023-10-09 11:08:19 -0300

    09.10.23 x4

--- 2-SegundoSemestre/1-TecnicasProgramacao/1-Aulas/8-Aula 8 (09.10.23)/2-Exercicios/3-Exercicio/main.c
+++ 2-SegundoSemestre/1-TecnicasProgramacao/1-Aulas/8-Aula 8 (09.10.23)/2-Exercicios/3-Exercicio/main.c
@@ -2,25 +2,33 @@
 #include <stdlib.h>
 #include <locale.h>
 
-
 int main() {
 	setlocale(LC_ALL,"");
-	int mat [3][4];
+	int mat [3][3];
 	//coluna |linha
-	int i, j;
-	
+	int i=0, j=0;
 	for(j=0;j<=2;j++){
 		for(i=0;i<=3;i++){
-			
-			printf("\nInforme um número: ");
+			printf("\nInforme um valor: ");
 			scanf("%d",&mat[j][i]);
 		}
 	}
+	printf("\nMatriz: \n");
+	for(j=0;j<=2;j++){
+		for(i=0;i<=3;i++){
+
+			printf(" %d ", mat[j][i]);
+		}
+		printf("\n");
+	}
+	
+	printf("\nMatriz transposta: \n");
 	for(j=0;j<=2;j++){
 		for(i=0;i<=3;i++){
 			
-			printf(" %d " , mat[j][i]);
-		
+			
+			
+			printf(" %d ", mat[j][i]);
 		}
 		printf("\n");
 	}
