a63bcdfec6edb30fe51ef00b2f299ab923815a24
GABRIEL ALBUQUERQUE DE MOURA SILVA <2560245@fmu.interno>
2023-10-09 11:16:01 -0300

    09.10.23 x5

--- 2-SegundoSemestre/1-TecnicasProgramacao/1-Aulas/8-Aula 8 (09.10.23)/2-Exercicios/3-Exercicio/main.c
+++ 2-SegundoSemestre/1-TecnicasProgramacao/1-Aulas/8-Aula 8 (09.10.23)/2-Exercicios/3-Exercicio/main.c
@@ -5,6 +5,7 @@
 int main() {
 	setlocale(LC_ALL,"");
 	int mat [3][3];
+	int mat2[3][3];
 	//coluna |linha
 	int i=0, j=0;
 	for(j=0;j<=2;j++){
@@ -23,12 +24,12 @@ int main() {
 	}
 	
 	printf("\nMatriz transposta: \n");
-	for(j=0;j<=2;j++){
-		for(i=0;i<=3;i++){
+	for(i=0;i<=2;i++){
+		for(j=0;j<=3;j++){
+			mat2[j][i] = mat[i][j];
 			
-			
-			
-			printf(" %d ", mat[j][i]);
+
+			printf(" %d ", mat2[i][j]);
 		}
 		printf("\n");
 	}
