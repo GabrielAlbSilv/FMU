3a3cf2845bd88b9de870b84e8fa4f2528afa31b7
GABRIEL ALBUQUERQUE DE MOURA SILVA <2560245@fmu.interno>
2023-11-06 11:04:23 -0300

    06.11.23 x7

--- 2-SegundoSemestre/1-TecnicasProgramacao/1-Aulas/11-Aula 11 (06.11.23)/2-Exercicios/1-Exercicio1/main.c
+++ 2-SegundoSemestre/1-TecnicasProgramacao/1-Aulas/11-Aula 11 (06.11.23)/2-Exercicios/1-Exercicio1/main.c
@@ -12,12 +12,12 @@ int main() {
 		printf("\nDigite um número: ");
 		scanf("%d",&val);
 		if(val == 0){
+			fclose(arq);
 			return 0;
 		}
 		fprintf(arq,"%d\n",val);
 	}
 	printf("\nArquivo gravado com sucesso!");
 	fclose(arq);
-	
 	return 0;
 }
