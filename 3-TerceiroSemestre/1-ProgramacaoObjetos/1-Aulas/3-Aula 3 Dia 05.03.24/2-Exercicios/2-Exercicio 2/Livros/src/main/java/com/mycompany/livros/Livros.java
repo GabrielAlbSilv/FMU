9bef7ec3a0ab08ef7e3d1b192d1be491fc0aafb3
GabrielAlbSilv <gabrielalbuquerquemoura@gmail.com>
2024-03-05 11:11:14 -0300

    commit x5

--- 3-TerceiroSemestre/1-ProgramacaoObjetos/1-Aulas/3-Aula 3 Dia 05.03.24/2-Exercicios/2-Exercicio 2/Livros/src/main/java/com/mycompany/livros/Livros.java
+++ 3-TerceiroSemestre/1-ProgramacaoObjetos/1-Aulas/3-Aula 3 Dia 05.03.24/2-Exercicios/2-Exercicio 2/Livros/src/main/java/com/mycompany/livros/Livros.java
@@ -4,17 +4,17 @@ package com.mycompany.livros;
 
 public class Livros {
 
-   String título, autor,ano;
+   String titulo, autor,ano;
     int paginas;
     public Livros (String titulo, String autor, String ano, int paginas){
         super();
-        this.título = titulo;
+        this.titulo = titulo;
         this.autor = autor;
         this.ano = ano;
         this.paginas = paginas;
      }
     void dadosLivros(){
-        System.out.println("\nTítulo do livro: "+título);
+        System.out.println("\nTítulo do livro: "+titulo);
         System.out.println("Autor: "+autor);
         System.out.println("Ano de publicação: "+ano);
         System.out.println("Quantidade de páginas: "+paginas);
