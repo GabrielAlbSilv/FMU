3823659f458580bfa183a6924cbfe470d926095c
GabrielAlbSilv <gabrielalbuquerquemoura@gmail.com>
2024-03-05 11:06:05 -0300

    commit x4

---
+++ 3-TerceiroSemestre/1-ProgramacaoObjetos/1-Aulas/3-Aula 3 Dia 05.03.24/2-Exercicios/2-Exercicio 2/Livros/src/main/java/com/mycompany/livros/main.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.livros;
+
+
+public class main {
+    public static void main(String[] args){
+    Livros livros=new Livros("It: A Coisa","Stephen King","2014",1104);
+    Livros livros2=new Livros("Pálido ponto azul ","Carl Segan","2019",336);
+    Livros livros3=new Livros("Fogo & Sangue – Volume 1 ","George R. R. Martin","2018",664);
+    livros.dadosLivros();
+    livros2.dadosLivros();
+    livros3.dadosLivros();
+    }
+}
