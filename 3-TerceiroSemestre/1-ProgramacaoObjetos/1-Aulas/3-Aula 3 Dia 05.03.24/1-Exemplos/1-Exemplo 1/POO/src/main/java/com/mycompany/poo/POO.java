85f074bcc9404b230dec6ccb2d8a9fc4e7986d39
GabrielAlbSilv <gabrielalbuquerquemoura@gmail.com>
2024-03-05 09:57:16 -0300

    commit x2

--- 3-TerceiroSemestre/1-ProgramacaoObjetos/1-Aulas/3-Aula 3 Dia 05.03.24/1-Exemplos/1-Exemplo 1/POO/src/main/java/com/mycompany/poo/POO.java
+++ 3-TerceiroSemestre/1-ProgramacaoObjetos/1-Aulas/3-Aula 3 Dia 05.03.24/1-Exemplos/1-Exemplo 1/POO/src/main/java/com/mycompany/poo/POO.java
@@ -4,18 +4,17 @@ package com.mycompany.poo;
 
 public class POO {
 
-    String nome;
+    String nome, cpf;
     int idade;
-    String cpf;
     public POO(String nome, int idade, String cpf){
-        super();
-        this.nome=nome;
+        super(); //construtor da super classe, super classe contém os dados abaixo
+        this.nome=nome; //instanciando variáel nome
         this.idade=idade;
         this.cpf=cpf;
     }
     void dadosPoo(){
         System.out.println("Nome da pessoa: "+nome);
         System.out.println("Idade da pessoa: "+idade);
-        
+        System.out.println("CPF da pessoa: "+cpf);
     }
 }
--- 3-TerceiroSemestre/1-ProgramacaoObjetos/1-Aulas/3-Aula 3 Dia 05.03.24/1-Exemplos/1-Exemplo 1/POO/src/main/java/com/mycompany/poo/main.java
+++ 3-TerceiroSemestre/1-ProgramacaoObjetos/1-Aulas/3-Aula 3 Dia 05.03.24/1-Exemplos/1-Exemplo 1/POO/src/main/java/com/mycompany/poo/main.java
@@ -2,5 +2,11 @@
 package com.mycompany.poo;
 
 public class main {
-    
+    public static void main(String[] args){
+    POO poo=new POO("Gabriel", 36, "xxxxxx");
+    POO poo2=new POO("Luke", 20, "yyyyyyyy");
+    System.out.println("Nome da pessoa: "+poo.nome+"\nIdade: "+poo.idade+"\nCPF: "+poo.cpf);
+    System.out.println("Nome da pessoa: "+poo2.nome+"\nIdade: "+poo2.idade+"\nCPF: "+poo2.cpf);
+    poo.dadosPoo();
+    }
 }
